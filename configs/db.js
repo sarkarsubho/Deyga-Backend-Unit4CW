d32f287f9a411cf343ab926e39b6176117f84961
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2022-03-29 23:42:18 +0530

    ytyty

--- configs/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require("mongoose");
-
-module.exports=()=>{
-    return mongoose.connect(
-        "mongodb+srv://subhankar:subho0905@cluster0.zrx9z.mongodb.net/Deyga?retryWrites=true&w=majority"
-    )
-}

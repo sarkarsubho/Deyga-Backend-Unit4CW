8ceeec06478104fc51a36720cf6d2f291eeae67d
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2022-03-29 23:26:21 +0530

    hhds

--- allProduct/src/configs/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require("mongoose");
-
-module.exports=()=>{
-    return mongoose.connect(
-        "mongodb+srv://dhaval:dhaval_123@cluster0.ljuvz.mongodb.net/web15-fileuploads?retryWrites=true&w=majority"
-    )
-}

fe23295c4be8f18ee2f6cd6b41927c990b00dd21
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2022-04-01 10:32:00 +0530

    chhange index to homepage

--- fontend/Otp.js
+++ fontend/Otp.js
@@ -4,7 +4,7 @@
       console.log(otp)
       if(otp==value){
           alert("Your Order has been placed! Happy Buying");
-          window.location.href="homepage.html"
+          window.location.href="index.html"
       }
       else {
           alert("Invalid OTP")

6c7b01aba0084d0c1a1a7cb87ebd28f1806e1946
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2022-04-02 19:48:05 +0530

    signupand login page update

--- fontend/SignUp.js
+++ fontend/SignUp.js
@@ -1,3 +1,34 @@
+// import navbar from '/navbar.js'
+// let navbar_div=document.getElementById("navbar");
+// navbar_div.innerHTML=navbar()
+// import country_code from '/Data.js'
+// import footer from '/footer.js'
+// let country_div=document.getElementById("country-code");
+// country_div.innerHTML=country_code()
+// let footer_div=document.getElementById("footer");
+// footer_div.innerHTML=footer()
+
+// document.querySelector("form").addEventListener("submit",getData);
+// let dataArr=JSON.parse(localStorage.getItem("userArr"))||[];
+// function getData(event){
+//     event.preventDefault();
+//    let First=document.getElementById("first").value;
+//    let last=document.getElementById("last").value
+//    let email=document.getElementById("eml").value
+//    let pass=document.getElementById("pass").value;
+//    let objData={
+//        First:First,
+//        Last:last,
+//        Email:email,
+//        Pass:pass
+//    }
+//    dataArr.push(objData);
+//    localStorage.setItem("userArr",JSON.stringify(dataArr))
+//    alert("Your Details are successfully Submited");
+//    window.location.href="Login.html"
+// }
+
+
 import navbar from '/navbar.js'
 let navbar_div=document.getElementById("navbar");
 navbar_div.innerHTML=navbar()
@@ -10,20 +41,56 @@ footer_div.innerHTML=footer()
 
 document.querySelector("form").addEventListener("submit",getData);
 let dataArr=JSON.parse(localStorage.getItem("userArr"))||[];
-function getData(event){
-    event.preventDefault();
+async function getData(event){
+    try{
+        event.preventDefault();
    let First=document.getElementById("first").value;
    let last=document.getElementById("last").value
    let email=document.getElementById("eml").value
    let pass=document.getElementById("pass").value;
-   let objData={
-       First:First,
-       Last:last,
-       Email:email,
-       Pass:pass
+   var objData={
+       firstName:First,
+       lastName:last,
+       email:email,
+       password:pass
    }
-   dataArr.push(objData);
-   localStorage.setItem("userArr",JSON.stringify(dataArr))
-   alert("Your Details are successfully Submited");
-   window.location.href="Login.html"
+   ,
+   objData = JSON.stringify(objData);
+    }
+    catch(err)
+    {
+        console.log(err.message);
+    }
+
+    let reg_api = `https://authodegya.herokuapp.com/registeration`;
+
+    let resp = await fetch(reg_api , {
+        method : "POST",
+        body : objData,
+        headers : {
+            "content-Type" : "application/json"
+        }
+    })
+
+    let data = await resp.json();
+    console.log(data);
+    if(data.error == false)
+    {
+        console.log(data.registeration.firstName)
+        alert("Registeration succesful");
+        window.location.href="Login.html"
+
+    }
+    else{
+        console.log(err.message)
+        alert("user not exist !")
+    }
+
+//    dataArr.push(objData);
+//    localStorage.setItem("userArr",JSON.stringify(dataArr))
+//    alert("Your Details are successfully Submited");
+   
 }
+
+
+
--- fontend/login.js
+++ fontend/login.js
@@ -1,3 +1,48 @@
+// import navbar from '/navbar.js'
+// let navbar_div=document.getElementById("navbar");
+// navbar_div.innerHTML=navbar()
+// import country_code from '/Data.js'
+// let country_div=document.getElementById("country-code");
+// country_div.innerHTML=country_code()
+// import footer from '/footer.js'
+// let footer_div=document.getElementById("footer");
+// footer_div.innerHTML=footer()
+
+
+// document.querySelector("form").addEventListener("submit",log);
+// var regUser=JSON.parse(localStorage.getItem("userArr"));
+// console.log(regUser)
+// // console.log(regUser)
+// function log(event){
+//     event.preventDefault();
+//     var em=document.querySelector("#inps").value;
+//     var pass=document.querySelector("#inp").value;
+//     // console.log(em,pass)
+//        var flag=false;
+//        let first=0;
+//        let email=0;
+//     for(var i=0;i<regUser.length;i++){
+//         if((em==regUser[i].Email)&&(pass==regUser[i].Pass)){
+//             flag=true;
+//             first=regUser[i].First;
+//             email=regUser[i].Email;
+//         }
+//     }
+//     let dataArr=[];
+//     if(flag==true){
+//         let show=document.getElementById("show");
+//         show.textContent=first;
+//         dataArr.push(first,email)
+//        localStorage.setItem("regUsers",JSON.stringify(dataArr));
+//        alert("login Success.");
+//         window.location.href="allproduct.html";
+//     }
+//     else{
+//        alert("Your email or passworrd was incorrect")
+//     }
+// }
+
+
 import navbar from '/navbar.js'
 let navbar_div=document.getElementById("navbar");
 navbar_div.innerHTML=navbar()
@@ -7,37 +52,48 @@ country_div.innerHTML=country_code()
 import footer from '/footer.js'
 let footer_div=document.getElementById("footer");
 footer_div.innerHTML=footer()
+var logins = document.getElementById("login");
 
 
 document.querySelector("form").addEventListener("submit",log);
 var regUser=JSON.parse(localStorage.getItem("userArr"));
 console.log(regUser)
 // console.log(regUser)
-function log(event){
-    event.preventDefault();
+async function log(event){
+    try{
+        event.preventDefault();
     var em=document.querySelector("#inps").value;
     var pass=document.querySelector("#inp").value;
-    // console.log(em,pass)
-       var flag=false;
-       let first=0;
-       let email=0;
-    for(var i=0;i<regUser.length;i++){
-        if((em==regUser[i].Email)&&(pass==regUser[i].Pass)){
-            flag=true;
-            first=regUser[i].First;
-            email=regUser[i].Email;
+    var objData ={
+        email : em,
+        password : pass
+    }
+    objData = JSON.stringify(objData);
+    }
+    catch(err)
+    {
+        console.log(err.message);
+    }
+let reg_api = `https://authodegya.herokuapp.com/login`
+
+    let resp = await fetch(reg_api , {
+        method : "POST",
+        body : objData,
+        headers : {
+            "content-Type" : "application/json"
         }
+    })
+
+    let data = await resp.json();
+    let names = data.login.firstName;
+    console.log(data.login)
+    console.log(data.login.firstName)
+    localStorage.setItem("name" , JSON.stringify(names))
+    if(data.error==false)
+    {
+        alert("login sucess")
+        window.location.href="homepage.html"
     }
-    let dataArr=[];
-    if(flag==true){
-        let show=document.getElementById("show");
-        show.textContent=first;
-        dataArr.push(first,email)
-       localStorage.setItem("regUsers",JSON.stringify(dataArr));
-       alert("login Success.");
-        window.location.href="allproduct.html";
-    }
-    else{
-       alert("Your email or passworrd was incorrect")
-    }
+    
+   
 }
